adfbb35176ebc5d722095aff34c7bf5702ab715f
CY3ORG <prathamjain1335@gmail.com>
2025-08-20 00:21:37 +0530

    basic changes applied

---
+++ src/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+
+
+function Navbar() {
+    
+  return (
+    
+    
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/logo.png" alt="Logo" height="40" /> {/* Place your logo image in public/ as logo.png */}
+      </div>
+      <div className="navbar-links">
+        <a href="/">Home</a>
+        <a href="/chat">Chat</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div className="logreg">
+        <a href="/login">Login</a>
+        <a href="/login">Register</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+
+
